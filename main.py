6b206380ea30f7b13a07a8e5e2deb731b35d8494
Michael Auger <michaelauger@Michaels-MBP.attlocal.net>
2019-02-26 18:23:26 -0600

    Add class structure

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import os
 from file_reader import read_file
+from file_writer import write_output
 
 class PizzaSlicer:
-    def __init__(self, header, rows):
+
+    def __init__(self, header, rows, output_file):
+        self.output_file = output_file
         self.rows = header['rows']
         self.cols = header['cols']
         self.min_ingredients = header['minIngredients']
         self.maxCellsPerClice = header['maxCellsPerSlice']
         self.rows = []
+        self.slices = []
         for row in rows:
             self.rows.append(row['row'])
+
+    def get_num_slices(self):
+        return len(self.num_slices)
+
+    def get_slices_formatted(self):
+        formatted_slices = []
+        for slice in self.slices:
+            sliceStr = "{} {} {} {}".format(slice.row_start, slice.col_start, slice.row_end, slice.col_end)
+            formatted_slices.append(sliceStr)
+        return formatted_slices
+
+    def output_solution(self):
+        num_slices = self.get_num_slices()
+        formatted_slices = self.get_slices_formatted()
+        output = num_slices + formatted_slices
+        write_output(self.output_file, output)
 
 
 example_file = os.path.join('data', 'a_example.in')
@@ -22,8 +42,8 @@ small = read_file(small_file, ['rows', 'cols', 'minIngredients', 'maxCellsPerSli
 medium = read_file(medium_file, ['rows', 'cols', 'minIngredients', 'maxCellsPerSlice'], ['row'])
 big = read_file(big_file, ['rows', 'cols', 'minIngredients', 'maxCellsPerSlice'], ['row'])
 
-exampleSlicer = PizzaSlicer(example['header'], example['rows'])
-smallSlicer = PizzaSlicer(small['header'], small['rows'])
-mediumSlicer = PizzaSlicer(medium['header'], medium['rows'])
-bigSlicer = PizzaSlicer(big['header'], big['rows'])
+exampleSlicer = PizzaSlicer(example['header'], example['rows'], 'example.out')
+smallSlicer = PizzaSlicer(small['header'], small['rows'], 'small.out')
+mediumSlicer = PizzaSlicer(medium['header'], medium['rows'], 'medium.out')
+bigSlicer = PizzaSlicer(big['header'], big['rows'], 'big.out')
 
---
+++ slice.py
@@ -0,0 +1,6 @@
+class Slice:
+    def __init__(self, row_start, row_end, col_start, col_end):
+        self.row_start = row_start
+        self.row_end = row_end
+        self.col_start = col_start
+        self.col_end = col_end
